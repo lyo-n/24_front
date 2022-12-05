ae1c157e799adf856608a4884609db6975a29558
Ihor Yanda <62036865+lyo-n@users.noreply.github.com>
2022-12-06 01:25:53 +0200

    link_fix3

--- pages/accounts.jsx
+++ pages/accounts.jsx
@@ -105,7 +105,7 @@ const Accounts = () => {
         </InputGroup>
     </div>
         <InputGroup className="mb-3" style={{ width: '20%', margin: '0 auto', display: "flex"}}>
-            <Link href="https://two4-server.onrender.com/">
+            <Link href="/">
                 <Button variant="outline-success" id="button-addon1" style={{ width: '150px'}} 
                     onClick={handleCreate}
                 >
